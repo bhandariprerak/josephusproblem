aabb2a423e16c94dc63d9975465da7f8bf702b46
Prerak Bhandari <68781278+bhandariprerak@users.noreply.github.com>
2024-08-22 08:05:58 +0000

    commented the false test case

--- test_main.py
+++ test_main.py
@@ -3,5 +3,5 @@ import main
 def test_postion_to_stand():
     assert main.position_to_stand(1024) == 1 # true
     assert main.position_to_stand(41) == 19 # true
-    assert main.position_to_stand(5) == 2 # false
+    # assert main.position_to_stand(5) == 2 # false
     
