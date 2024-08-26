b8f0a73bc289504b8eaa7cbc167493c9b1231bf3
akshatrshah <66900011+akshatrshah@users.noreply.github.com>
2024-08-26 03:05:58 +0000

    Added a new False Test Case

--- test_main.py
+++ test_main.py
@@ -4,5 +4,4 @@ def test_postion_to_stand():
     assert main.position_to_stand(1024) == 1 # true
     assert main.position_to_stand(41) == 19 # true
     assert main.position_to_stand(5) == 2 # false
-    
-    
+    assert main.position_to_stand(44) == 30 # false
